f65945f4fcd0d54c96eabffd52a792128c6465da
Jaeho Yoo <y73447jh@gmail.com>
2024-10-24 23:20:39 +0900

    유저디폴트에 저장하는 Up/Down 예시 구현

--- LetsSwiftWidget/ContentView.swift
+++ LetsSwiftWidget/ContentView.swift
@@ -9,53 +9,25 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
 
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
+  @AppStorage("singleNumber") private var singleNumber: Int = .zero
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
+  var body: some View {
+    VStack {
+      GroupBox("@AppStorage 저장된 숫자") {
+        Text("\(singleNumber)")
+      }
+      .font(.largeTitle)
+      
+      HStack {
+        Button("UP!", action: { singleNumber += 1 })
+        Button("DOWN!", action: { singleNumber -= 1 })
+      }
+      .buttonStyle(.borderedProminent)
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+  }
 }
 
 #Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+  ContentView()
 }
