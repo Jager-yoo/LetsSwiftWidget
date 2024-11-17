f3461052e574c83af8015f27906af791996b3306
Jaeho Yoo <y73447jh@gmail.com>
2024-11-17 20:54:38 +0900

    인터랙티브 잠금 화면 위젯 추가

--- InteractiveWidget/InteractiveWidget.swift
+++ InteractiveWidget/InteractiveWidget.swift
@@ -33,7 +33,7 @@ struct InteractiveWidgetView: View {
   let entry: Provider.Entry
   
   @AppStorage("singleNumber", store: .shared) private var singleNumber: Int = .zero
-  @AppStorage("widgetShowsResult", store: .shared) var widgetShowsResult: Bool = false
+  @AppStorage("widgetShowsResult", store: .shared) private var widgetShowsResult: Bool = false
 
   var body: some View {
     if widgetShowsResult {
@@ -91,6 +91,61 @@ struct InteractiveWidgetView: View {
   }
 }
 
+struct LockScreenWidgetView: View {
+  let entry: Provider.Entry
+
+  @AppStorage("singleNumber", store: .shared) private var singleNumber: Int = .zero
+  @AppStorage("widgetShowsResult", store: .shared) private var widgetShowsResult: Bool = false
+
+  var body: some View {
+    if widgetShowsResult {
+      Button(intent: ResetIntent()) {
+        ZStack {
+          RoundedRectangle(cornerRadius: 16)
+            .fill(Color.green.opacity(0.5))
+
+          VStack {
+            Text("숫자 변경 성공")
+
+            Text("It's **'\(singleNumber)'** now")
+              .font(.system(size: 24, design: .rounded))
+          }
+        }
+      }
+      .buttonStyle(.plain)
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .transition(.blurReplace)
+    } else {
+      HStack {
+        Button(intent: NumberDownIntent()) {
+          Image(systemName: "minus")
+            .frame(width: 20, height: 20)
+        }
+        .buttonBorderShape(.circle)
+        .padding(4)
+
+        Spacer()
+
+        Text("\(singleNumber)")
+          .font(.system(size: 32, design: .rounded))
+          .invalidatableContent()
+          .contentTransition(
+            .numericText(value: Double(singleNumber))
+          )
+
+        Spacer()
+
+        Button(intent: NumberUpIntent()) {
+          Image(systemName: "plus")
+            .frame(width: 20, height: 20)
+        }
+        .buttonBorderShape(.circle)
+        .padding(4)
+      }
+    }
+  }
+}
+
 struct InteractiveWidget: Widget {
   let kind: String = "InteractiveWidget"
 
@@ -102,6 +157,20 @@ struct InteractiveWidget: Widget {
     .configurationDisplayName("Let's Swift!")
     .description("앱의 경계를 넘다: Interactive Widget")
     .contentMarginsDisabled()
+  }
+}
+
+struct InteractiveLockScreenWidget: Widget {
+  let kind: String = "InteractiveLockScreenWidget"
+
+  var body: some WidgetConfiguration {
+    StaticConfiguration(kind: kind, provider: Provider()) { entry in
+      LockScreenWidgetView(entry: entry)
+        .containerBackground(.fill.quinary, for: .widget)
+    }
+    .configurationDisplayName("Let's Swift!")
+    .description("앱의 경계를 넘다: Interactive Widget")
+    .supportedFamilies([.accessoryRectangular])
   }
 }
 
--- InteractiveWidget/InteractiveWidgetBundle.swift
+++ InteractiveWidget/InteractiveWidgetBundle.swift
@@ -13,5 +13,6 @@ struct InteractiveWidgetBundle: WidgetBundle {
   
   var body: some Widget {
     InteractiveWidget()
+    InteractiveLockScreenWidget()
   }
 }
